81fa5cc9a726af66c5d7df84155e05e0da264b3a
MariaAbra <maryiaabramuk@gmail.com>
2024-11-17 16:12:08 +0100

    .

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import sys
 import pygame as pg
 import random
+import pygame.mixer as mixer
+
+mixer.init()
 pg.init()
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0,)
@@ -16,13 +19,15 @@ PLATFORM_HEIGHT = 30
 BALL_WIDTH = 50
 BALL_HEIGHT = 50
 FPS = 30
-
-
-
+sound_over = pg.mixer.Sound('ay-menya-snaypnuli-v-polte.mp3')
+sound_collision = pg.mixer.Sound('est-probitie.mp3')
+sound_background = pg.mixer.Sound('otkryitie-filma-korotkaya-muzyika-korotkaya-muzyika-38490.mp3')
+sound_background.play(100)
 def game(screen,clock,assets):
     
 
     counter = 0
+    start_time = pg.time.get_ticks()
     PLATFORM_X = (SCREEN_WIDTH - PLATFORM_WIDTH) //2
     PLATFORM_Y =int(SCREEN_HEIGHT * 0.8)
     screen = pg.display.set_mode((SCREEN_WIDTH , SCREEN_HEIGHT))
@@ -61,21 +66,18 @@ def game(screen,clock,assets):
         BALL_X += speed_vector.x
         
 
-        #screen.fill(WHITE)
+    
         screen.blit(assets['background'], (0,0))
         
-        # platform = pg.Rect(PLATFORM_X, PLATFORM_Y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
     
-
-
-        # pg.draw.rect(screen ,YELLOW, platform)
+        
 
         ball = assets['ball'].get_rect()
         ball.x=BALL_X
         ball.y=BALL_Y
-        # ball = pg.Rect(BALL_X, BALL_Y, BALL_WIDTH, BALL_HEIGHT)
+
         screen.blit(assets['ball'],ball)
-        # pg.draw.rect(screen ,RED, ball)
+        
         platform = assets['panel'].get_rect()
         platform.x=PLATFORM_X
         platform.y=PLATFORM_Y
@@ -84,8 +86,11 @@ def game(screen,clock,assets):
         ball_center = (ball.x + ball.width/2, ball.y + ball.height/2)
         platform_center = (platform.x + platform.width/2, platform.y + platform.height/2)
         text_surface = def_font.render(str(counter) , False,  GREEN)
+        time_surface = def_font.render(str(int((pg.time.get_ticks()-start_time)/FPS)) , False,  GREEN)
         
         if ball.colliderect(platform):
+            
+            sound_collision.play()
             t = random.Random().random()/2
             collision_vector = (ball_center[0]- platform_center[0], ball_center[1]- platform_center[1])
             BALL_DIRECTION =pg.math.Vector2(collision_vector).normalize()
@@ -93,6 +98,7 @@ def game(screen,clock,assets):
         if ball.colliderect(TOP_BORDER):
             BALL_DIRECTION = BALL_DIRECTION. reflect(pg.math.Vector2(0,1))
         if ball.colliderect(BOTTOM_BORDER):
+            
             break
             BALL_DIRECTION =BALL_DIRECTION .reflect(pg.math.Vector2(0,-1))
         if ball.colliderect(LEFT_BORDER):
@@ -101,8 +107,10 @@ def game(screen,clock,assets):
             BALL_DIRECTION =BALL_DIRECTION .reflect(pg.math.Vector2(-1,0))
 
         screen.blit(text_surface,(20,20))
+        screen.blit(time_surface,(50,20))
         pg.display.flip()  
         clock.tick(FPS)
+    return counter
 
 
     
@@ -116,10 +124,12 @@ if __name__=='__main__':
     }
     clock_out= pg.time.Clock()
     while True:
-        
-        game(screen_out,clock_out,assets)
-        finish_text = def_font.render('GAME OVER' , False ,(GREEN))
-        screen_out.blit(finish_text, (SCREEN_WIDTH/2-60, SCREEN_HEIGHT/2))
+        sound_background.play(100)
+        count_final = game(screen_out,clock_out,assets)
+        finish_text = def_font.render('GAME OVER ! press R to restart ' , False ,(GREEN))
+        sound_background.stop()
+        sound_over.play()
+        screen_out.blit(finish_text, (SCREEN_WIDTH/6-20, SCREEN_HEIGHT/2))
         pg.display.flip()
         while True:
             for event in pg.event.get():
